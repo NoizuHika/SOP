7160910e954a05dfa69263a599029f3b45a9a234
NoizuHika <47974646+NoizuHika@users.noreply.github.com>
2021-06-07 12:47:21 +0200

    Update run.cpp

--- 31.05/run.cpp
+++ 31.05/run.cpp
@@ -2,10 +2,20 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+
+pid_t cpid;
+
+void handler(int i){
+  kill(cpid, i);
+}
 
 auto main(int argc, char *argv[]) -> int {
 
-  auto cpid = fork();
+  cpid = fork();
   if (cpid == -1) {
     perror("fork");
     exit(EXIT_FAILURE);
@@ -16,13 +26,17 @@ auto main(int argc, char *argv[]) -> int {
     exit(EXIT_FAILURE);
   }
 
+  struct sigaction act{};
+  act.sa_handler = handler;
+
+  sigaction(SIGTERM, &act, nullptr);
+
   signal(SIGINT, SIG_IGN);
   int status = 0;
   waitpid(cpid, &status, 0);
 
   if (WIFEXITED(status)) {
-    std::cout << "Child PID is %d proccess, status: %d\n", cpid,
-        WEXITSTATUS(status) << "\n";
+    std::cout << "Child PID is " << cpid << " proccess, status: " << WEXITSTATUS(status) << "\n";
   }
   if (WIFSIGNALED(status)) {
     std::cout << "Procces: " << cpid
