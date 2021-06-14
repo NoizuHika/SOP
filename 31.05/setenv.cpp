f6b3ff6f0a2a636a0a486ed001128cf27d90bf84
NoizuHika <47974646+NoizuHika@users.noreply.github.com>
2021-06-14 11:58:55 +0200

    Update setenv.cpp

--- 31.05/setenv.cpp
+++ 31.05/setenv.cpp
@@ -1,11 +1,6 @@
 #include <iostream>
-#include <sys/types.h>
-#include <sys/wait.h>
 #include <unistd.h>
-#include <stdio.h>
 #include <stdlib.h>
-#include <signal.h>
-#include <string.h>
 
 auto main(int argc, char* argv[]) -> int {
 
